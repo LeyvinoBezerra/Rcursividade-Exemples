15872e133583cb6d9c38e8e315ffbde6487d8f50
Leyvino Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2023-02-12 15:35:00 -0300

    init somatorio

---
+++ src/recursividade/Somatorio.java
@@ -0,0 +1,17 @@
+package recursividade;
+
+public class Somatorio {
+
+	public static void main(String[] args) {
+	
+    System.out.println(somatorio(5));
+
+	}
+public static int somatorio(int num) { 
+	
+	if(num == 1) {
+		return 1;
+	}
+	return num + somatorio(num-1);
+}
+}
