fafab8541d71f129ae0f19724901385ce1b63dc3
Leyvino Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2023-02-12 16:32:50 -0300

    end day

--- src/recursividade/Somatorio.java
+++ src/recursividade/Somatorio.java
@@ -3,7 +3,7 @@ package recursividade;
 public class Somatorio {
 
 	public static void main(String[] args) {
-	
+
     System.out.println(somatorio(5));
 
 	}
---
+++ src/recursividade/SomatorioRecursivo.java
@@ -0,0 +1,37 @@
+package recursividade;
+
+import java.util.Scanner;
+
+public class SomatorioRecursivo {
+	
+	public static int somatorio(int n)
+	{
+	    int r;   
+
+		System.out.printf("n = %d \n",   n);
+
+	    if(n < 1) 
+	    {
+	    	System.out.printf("fim \n");
+			r = 0;
+	    }
+	    else 
+	    {
+	        r = n + somatorio(n-1);
+	    }
+	    
+	    return r;
+	}
+
+	
+      public static void main(String[] args) {
+	
+	Scanner in = new Scanner(System.in);
+    System.out.println("-------FATORIAL RECUSSIVO-----------");
+    System.out.println("------- Digite um numero !----------");
+    int exa = in.nextInt();
+    
+    System.out.printf("Somatorioa = %d", somatorio(exa));
+    
+	}
+}
